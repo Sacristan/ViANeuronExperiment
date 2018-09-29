f263027d932199d31cde354fc4c7419e2af25443
Sacristan <girts.kesteris@gmail.com>
2018-09-29 17:16:54 +0300

    Initial commit

---
+++ Assets/RandomAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationController : MonoBehaviour {
+
+
+    Animator _animator;
+	void Start () {
+
+        _animator = GetComponent<Animator>();
+        AssignRandomAnimation();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F)) AssignRandomAnimation();
+	}
+
+    private void AssignRandomAnimation()
+    {
+        _animator.SetTrigger("stopAnim");
+        int animId = Random.Range(0, 10);
+        _animator.SetInteger("AnimID", animId);
+
+    }
+}
